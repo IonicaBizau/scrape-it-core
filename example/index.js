f93a9e34fa717caa3793f0e592ae5f6669be7081
Ionică Bizău <bizauionica@gmail.com>
2021-01-29 13:27:40 +0200

    :arrow_up: 1.0.0 :tada:

--- example/index.js
+++ example/index.js
@@ -1,5 +1,45 @@
 "use strict";
 
-const scrapeItCore = require("../lib");
+const scrapeIt = require("../lib")
 
-console.log(scrapeItCore());
+// Promise interface
+const data = scrapeIt(`
+    <h1 class='heading'>Hello World</h1>
+    <img src="test.jpg" />
+    <p>Lorem ipsum dolor sit amet...</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li><i>6</i></li>
+    </ul>
+`, {
+    title: ".heading"
+  , desc: "p"
+  , avatar: {
+        selector: "img"
+      , attr: "src"
+    }
+  , items: {
+        listItem: "ul > li"
+      , data: {
+            content: {
+                how: "text"
+            }
+        }
+    }
+})
+
+console.log(data)
+// { title: 'Hello World',
+//   desc: 'Lorem ipsum dolor sit amet...',
+//   avatar: 'test.jpg',
+//   items:
+//    [ { content: '1' },
+//      { content: '2' },
+//      { content: '3' },
+//      { content: '4' },
+//      { content: '5' },
+//      { content: '6' } ] }
